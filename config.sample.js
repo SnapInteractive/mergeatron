7133f937a124a11e4141e82d629e23eadfe4e801
chr0n1x <heilong24@gmail.com>
2013-12-09 10:47:24 -0500

    config.sample.js: whitespace (softtabs)

--- config.sample.js
+++ config.sample.js
@@ -1,62 +1,62 @@
 // Rename, or copy, to config.js in the same directory
 exports.config = {
-	db: {
-		type: 'mongo',
-		auth: {
-			user:    'username',
-			pass:    'password',
-			host:    'your.host',
-			port:    27017,
+    db: {
+        type: 'mongo',
+        auth: {
+            user:    'username',
+            pass:    'password',
+            host:    'your.host',
+            port:    27017,
             db:      'mergeatron',
             slaveOk: true
-		},
-		database: 'mergeatron',
-		collections: [ 'pulls' ]
-	},
-	log_level: 'info',
-	plugin_dirs: [ './plugins/' ],
-	plugins: {
-		github: {
-			method: 'hooks',    // 'hooks' for webhooks or 'polling' to poll the REST API
-			auth: {
-				type: 'basic',
-				username: 'username',
-				password: 'password'
-			},
-			user: 'user-to-watch',
-			repos: [ 'repo_name' ],
-			retry_whitelist: [ 'user', 'user2' ],    // optional whitelist of those able to trigger retries
-			skip_file_listing: false,
-			frequency: 15000,    // only necessary if method is 'polling'
-			port: '8888',        // only necessary if method is 'hooks'
-			// optional. If running GitHub Enterprise this is the host/port to access the REST API.
-			// Can be left out if just using github.com.
-			api: {
-				host: 'ghe.example.com',
-				port: '1234'
-			}
-		},
-		jenkins:  {
-			user: false,
-			pass: false,
-			protocol: 'http',
-			host: 'jenkins.yoururl.com:8080',
-			projects: [{
-				name: 'project_name',
-				repo: 'repo_name',
-				token: 'token',
-				rules: [ new RegExp(/.php/g) ]
-			}],
-			frequency: 2000
-		},
-		phpcs: {
-			enabled: false,
-			artifact: 'artifacts/phpcs.csv'
-		},
-		phpunit: {
-			enabled: false,
-			artifact: 'artifacts/junit.xml',
-			failure_limit: 3
-		}
-	}
+        },
+        database: 'mergeatron',
+        collections: [ 'pulls' ]
+    },
+    log_level: 'info',
+    plugin_dirs: [ './plugins/' ],
+    plugins: {
+        github: {
+            method: 'hooks',    // 'hooks' for webhooks or 'polling' to poll the REST API
+            auth: {
+                type: 'basic',
+                username: 'username',
+                password: 'password'
+            },
+            user: 'user-to-watch',
+            repos: [ 'repo_name' ],
+            retry_whitelist: [ 'user', 'user2' ],    // optional whitelist of those able to trigger retries
+            skip_file_listing: false,
+            frequency: 15000,    // only necessary if method is 'polling'
+            port: '8888',        // only necessary if method is 'hooks'
+            // optional. If running GitHub Enterprise this is the host/port to access the REST API.
+            // Can be left out if just using github.com.
+            api: {
+                host: 'ghe.example.com',
+                port: '1234'
+            }
+        },
+        jenkins:  {
+            user: false,
+            pass: false,
+            protocol: 'http',
+            host: 'jenkins.yoururl.com:8080',
+            projects: [{
+                name: 'project_name',
+                repo: 'repo_name',
+                token: 'token',
+                rules: [ new RegExp(/.php/g) ]
+            }],
+            frequency: 2000
+        },
+        phpcs: {
+            enabled: false,
+            artifact: 'artifacts/phpcs.csv'
+        },
+        phpunit: {
+            enabled: false,
+            artifact: 'artifacts/junit.xml',
+            failure_limit: 3
+        }
+    }
 };
